a8cab1a5872039c6407265d9f6a44bedbfb98497
Zanidd <ninijay93@gmail.com>
2019-05-03 17:44:38 +0200

    updated snapshot script

--- .config/scripts/snapshot.sh
+++ .config/scripts/snapshot.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
-lvcreate -L 500m -s /dev/mapper/main-root -n lv_root
-lvcreate -L 100m -s /dev/mapper/main-home -n lv_home
+# ca 10% of actual size used for snapshot
+rootsize=30G
+homesize=10G
+
+rootname=root_snap
+homename=home_snap
+
+# Remove snapshots if they exist
+
+sudo lvs | grep $rootname
+if [ $? -eq 0 ];
+then
+    sudo lvremove /dev/main/$rootname -y
+fi
+
+sudo lvs | grep $homename
+if [ $? -eq 0 ];
+then
+    sudo lvremove /dev/main/$homename -y
+fi
+
+# calc size
+
+sudo lvcreate -L $rootsize -s /dev/mapper/main-root -n $rootname
+sudo lvcreate -L $homesize -s /dev/mapper/main-home -n $homename
+
